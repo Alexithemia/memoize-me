93092470c9c5ea185ba2806872ebb62867dc11d5
Tyler <alexithemia@gmail.com>
2019-02-11 10:02:08 -1000

    Add query selector

--- memoize.js
+++ memoize.js
@@ -14,10 +14,33 @@ const memoizedById = function () {
   }
 }
 
+const memoizedQueryBy = function () {
+  let cache = {};
+  return function (id) {
+    if (id in cache) {
+      console.log('Fetching from cache');
+      return cache[id];
+    }
+    else {
+      console.log('Querying Element');
+      let result = document.querySelector(id);
+      cache[id] = result;
+      return result;
+    }
+  }
+}
+
 const findById = memoizedById();
+const queryMem = memoizedQueryBy();
 
 console.log(findById('cookie'));
 console.log(findById('cookie'));
 
 console.log(findById('cake'));
 console.log(findById('cake'));
+
+console.log(queryMem('#cookie'));
+console.log(queryMem('#cookie'));
+
+console.log(queryMem('#cake'));
+console.log(queryMem('#cake'));
